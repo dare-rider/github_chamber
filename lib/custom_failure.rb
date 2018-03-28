cfd5e031df3eda4522adfd255589c112a4aa9b6d
hsaini734 <hsaini734@gmail.com>
2018-03-28 12:02:41 +0530

    fixing authenticate_user! redirect

---
+++ lib/custom_failure.rb
@@ -0,0 +1,15 @@
+class CustomFailure < Devise::FailureApp
+  def redirect_url
+    new_user_session_url(:subdomain => 'secure')
+  end
+
+  # You need to override respond to eliminate recall
+  def respond
+    if http_auth?
+      http_auth
+    else
+      flash[:alert] = i18n_message unless flash[:notice]
+      redirect_to root_url
+    end
+  end
+end
